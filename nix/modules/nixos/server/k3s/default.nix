ab9b76e44fad07274928cebfea67b8c4a33821db
rhodate <16692923+Rhodate@users.noreply.github.com>
2025-04-01 23:05:18 -0400

    Use private networking :)

--- nix/modules/nixos/server/k3s/default.nix
+++ nix/modules/nixos/server/k3s/default.nix
@@ -38,54 +38,18 @@ with lib; {
         "*":
     '';
 
-    swarm.esh.templates.k3sEnvironment = {
-      # Split up the cluster and service CIDRs between the available range
-      template = with pkgs; ''
-        <%
-          parentCidr=$(${ndisc6}/bin/rdisc6 -1q ${config.swarm.hardware.networking.networkDevice});
-          prefixLengths=(${toString config.swarm.server.k3s.clusterCidrPrefixLength})
-          hostIds=(${concatStringsSep " " lib.swarm.allMachineHostIds})
-          splitCidrs=$( ${swarm.ipv6-splitter}/bin/ipv6-splitter $parentCidr ''${prefixLengths[@]} ''${hostIds[@]})
-          cidrs=($(
-            awk -f ${writeText "ipv6-splitter.awk" ''
-          BEGIN { found_match = 0 }
-          /Seed: ${config.networking.hostId}/ { found_match = 1; next }
-          found_match && /^[[:space:]]/ { print }
-          found_match && /^[^[:space:]]/ { exit }
-        ''} <<< ''${splitCidrs}
-          ))
-        %>
-        parentCidr=<%= ''${parentCidr} %>
-        cidrs=<%= ''${cidrs} %>
-        splitCidrs=<%= ''${splitCidrs} %>
-        clusterCidr=<%= ''${cidrs:0:1} %>
-      '';
-      destination = "/etc/rancher/k3s/cidrs";
-    };
-
     services.k3s = {
       enable = true;
       role = "server";
       extraFlags = toString [
         "--disable metrics-server"
-        "--cluster-cidr=\${clusterCidr}"
+        "--flannel-ipv6-masq"
+        "--cluster-cidr='fd02::/56'"
         "--service-cidr='fd01::/112'"
-        "--flannel-backend=none"
-        "--kube-controller-manager-arg=node-cidr-mask-size=${toString config.swarm.server.k3s.clusterCidrPrefixLength}"
         (concatStringsSep " " (concatMap (san: ["--tls-san" san]) config.swarm.server.k3s.san))
       ];
-      environmentFile = config.swarm.esh.templates.k3sEnvironment.destination;
       tokenFile = config.sops.secrets.k3s-token.path;
       clusterInit = config.swarm.server.k3s.clusterInit;
-    };
-
-    systemd.services.k3s = {
-      after = [
-        config.systemd.services.k3sEnvironment-swarm-template.name
-      ];
-      wants = [
-        config.systemd.services.k3sEnvironment-swarm-template.name
-      ];
     };
 
     networking.firewall = {
